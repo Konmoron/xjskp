2ef5e5f464dc62e0799f55db1e10b6e510512872
chaoqiangc <chaoqiangc@yonyou.com>
2025-04-21 02:46:26 +0800

    refactor(common_operations): 添加延时参数优化技能选择逻辑
    在技能选择函数中增加延时参数，避免操作过快导致识别失败，并统一延时设置以提高代码可维护性。

--- modules/operators/common_operations.py
+++ modules/operators/common_operations.py
@@ -116,23 +116,26 @@ def close_offline():
 # 选择技能
 # 主要选择能量系技能，这样和枪配合，任何boss都不怕
 def select_ji_neng():
-    if find_and_click('images/ji_neng/ji_guang.png'):
+    time.sleep(1)
+    before_sleep = 0.01
+    after_sleep = 0.01
+    if find_and_click('images/ji_neng/ji_guang.png', before_sleep=before_sleep, after_sleep=after_sleep):
         logger.info(f"发现并选择【激光技能】")
-    elif find_and_click('images/ji_neng/she_xian.png'):
+    elif find_and_click('images/ji_neng/she_xian.png', before_sleep=before_sleep, after_sleep=after_sleep):
         logger.info(f"发现并选择【射线技能】")
-    elif find_and_click('images/ji_neng/qiang_lian_fa.png'):
+    elif find_and_click('images/ji_neng/qiang_lian_fa.png', before_sleep=before_sleep, after_sleep=after_sleep):
         logger.info(f"发现并选择【枪-连发-技能】")
-    elif find_and_click('images/ji_neng/qiang_fen_lie_4.png'):
+    elif find_and_click('images/ji_neng/qiang_fen_lie_4.png', before_sleep=before_sleep, after_sleep=after_sleep):
         logger.info(f"发现并选择【枪-4分裂-技能】")
-    elif find_and_click('images/ji_neng/qiang_fen_lie.png'):
+    elif find_and_click('images/ji_neng/qiang_fen_lie.png', before_sleep=before_sleep, after_sleep=after_sleep):
         logger.info(f"发现并选择【枪-2分裂-技能】")
-    elif find_and_click('images/ji_neng/qiang_zeng_shang.png'):
+    elif find_and_click('images/ji_neng/qiang_zeng_shang.png', before_sleep=before_sleep, after_sleep=after_sleep):
         logger.info(f"发现并选择【枪-增伤-技能】")
-    elif find_and_click('images/ji_neng/qiang_bao_zha.png'):
+    elif find_and_click('images/ji_neng/qiang_bao_zha.png', before_sleep=before_sleep, after_sleep=after_sleep):
         logger.info(f"发现并选择【枪-爆炸-技能】")
     # elif find_and_click('images/ji_neng/wen_ya_dan_lian_fa.png'):
     #     logger.info(f"发现并选择【温压弹连发技能】images/ji_neng/wen_ya_dan_lian_fa.png")
-
+    time.sleep(1)
 def close_ji_neng_jiao_yi():
     if find_and_click('images/huan_qiu/ji_neng_jiao_yi.png', offset_name='close_ji_neng_jiao_yi'):
         logger.info(f"发现【技能交易】图片, 执行 - 关闭技能交易")
